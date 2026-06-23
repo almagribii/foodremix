7dca8a4af17ff039e97eee0ba4dab339202e61e0
Almagribii <brucadalm@gmail.com>
2026-06-23 18:57:04 +0700

    added game page

---
+++ app/dashboard/game/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Game - Foodremix",
+  description:
+    "Halaman ini berisi tentang game yang bisa dimainkan pengguna ",
+};
+
+export default function NotifPage() {
+  return(
+    <div>
+        Game Page
+    </div>
+
+  )
+}
